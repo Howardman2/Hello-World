827831fe887ad402045ba6944791b6475bec4e12
Howardman2 <90816995+Howardman2@users.noreply.github.com>
2021-09-16 00:19:36 -0400

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,20 @@
+print('Welcome to TRUE or FALSE')
+print ('Please answer with yes or no')
+Truth = input('Mr Gallo is the best: ')
+if Truth !='yes': 
+    print('wrong')
+if Truth =='yes': 
+    print('Correct!')
+Falth = input('Coding is dumb: ')
+if Falth =='yes':
+    print('WRONG')
+if Falth !='yes':
+    print('Correct!')
+Truce = input('Technology is the future: ')
+if Truce !='yes':
+    print('WRONGGGG')
+if Truce =='yes':
+    print('CORRRECTTT')
+
+print('Thanks for playing!!!!!!')
+
